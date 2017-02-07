446e8ef412344454718936b944f1c253681b1035
jezzamon <jezzamondev@gmail.com>
2017-02-06 23:06:06 -0500

    refactor to create the <race> component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RacesComponent } from './races/races.component';
+import { RaceService } from './races/race.service';
+import { RaceComponent } from './race/race.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RacesComponent,
+    RaceComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ RaceService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
